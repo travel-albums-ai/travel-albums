ac9de5c2f944f4b10df8eed44154ab633da2cb89
practiseo <practiseo@gmail.com>
2026-08-01 14:31:49 +0200

    feat: add Vercel analytics and speed insights to the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import AppRoutes from '@/components/AppRoutes';
 import '@/lib/i18n';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import 'leaflet/dist/leaflet.css';
 import { createRoot } from 'react-dom/client';
 import AppProviders from './context/AppProviders';
+
 import './freakflags.css';
 import './index.css';
 
@@ -15,6 +18,8 @@ const root = (window as any).__ROOT__ ??= createRoot(container);
 
 root.render(
   <QueryClientProvider client={queryClient}>
+    <SpeedInsights />
+    <Analytics />
     <AppProviders>
       <AppRoutes />
     </AppProviders>
