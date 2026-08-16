d5fd5ca276dc6215719594df598aa8d867e0fa96
practiseo <practiseo@gmail.com>
2026-08-17 01:27:05 +0200

    feature: remove GoToSettingsToggle component and its meta file

--- src/tools/MixedTools/GoToSettingsToggle.tsx
+++
@@ -1,9 +0,0 @@
-import PageRedirect from '@/components/PageRedirect';
-import { Settings } from 'lucide-react';
-import { useTranslation } from 'react-i18next';
-
-export default function GoToSettingsToggle() {
-  const { t } = useTranslation()
-
-  return <PageRedirect title={t('settings')} to="/settings" icon={<Settings size={16} />} skipTitle />
-}
