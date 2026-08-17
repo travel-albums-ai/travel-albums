658aa205978a4e6d391379780e4e1cb94d1ed0cb
practiseo <practiseo@gmail.com>
2026-08-17 12:27:10 +0200

    refactor: optimize LightboxWindow component structure and improve photo handling

--- src/windows/LightboxWindow.tsx
+++ src/windows/LightboxWindow.tsx
@@ -3,6 +3,7 @@ import { useSections_GLOBAL } from '@/context/globals/sectionsStore';
 import { useSettings, useSettingsStoreSelector } from '@/context/settingsStore';
 import { composeUrl } from '@/lib/thumbnailService';
 import { Box } from '@mui/material';
+import { useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { Lightbox } from 'yet-another-react-lightbox';
 import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
@@ -10,48 +11,87 @@ import Slideshow from 'yet-another-react-lightbox/plugins/slideshow';
 import Zoom from 'yet-another-react-lightbox/plugins/zoom';
 
 export default function LightboxWindow() {
-  const lightboxOpen = useSettingsStoreSelector(s => s.lightboxOpen)
-  const { setSetting } = useSettings()
-  const { setPreviewPhotoObj } = useSettings();
+  const lightboxOpen = useSettingsStoreSelector(s => s.lightboxOpen);
+  const previewPhotoObj = useSettingsStoreSelector(s => s.previewPhotoObj);
 
-  const previewPhotoObj = useSettingsStoreSelector((state) => state.previewPhotoObj)
-  const photo = previewPhotoObj ?? null
+  const { setSetting, setPreviewPhotoObj } = useSettings();
 
-  const { type_name = '', id = '' } = useParams()
-  const sections = useSections_GLOBAL()
-  const photosFiltered = useFilteredPhotos_GLOBAL()
+  const { type_name = '', id = '' } = useParams();
 
-  const showAll = type_name === ''
+  const sections = useSections_GLOBAL();
+  const photosFiltered = useFilteredPhotos_GLOBAL();
 
-  const foundSection = sections?.find((s) => s.type === type_name)
-  const foundSet = foundSection?.data?.find((d: any) => d.name === id)
-  const photos = showAll ? photosFiltered : foundSet?.photos || []
+  const showAll = type_name === '';
 
-  const showWindow = lightboxOpen === true
+  const foundSection = sections?.find((s) => s.type === type_name);
+  const foundSet = foundSection?.data?.find((d: any) => d.name === id);
+
+  const photos = showAll
+    ? photosFiltered
+    : foundSet?.photos || [];
+
+  const showWindow = lightboxOpen === true;
+
+  const initialIndex = useMemo(() => {
+    if (!previewPhotoObj || photos.length === 0) {
+      return 0;
+    }
+
+    return photos.findIndex(p => p === previewPhotoObj);
+  }, [photos, previewPhotoObj]);
+
+  const slides = useMemo(
+    () =>
+      photos.map(p => ({
+        src: composeUrl(p, true),
+      })),
+    [photos],
+  );
 
   if (!showWindow) {
-    return null
+    return null;
   }
 
-  const updatePreviewPhoto = (index: number) => {
-    setPreviewPhotoObj(photos[index])
-  }
-
-  return <>
-    <Box sx={{ zIndex: 1, position: 'relative', top: 0, left: 0, width: '100%', height: '100%' }}>
+  return (
+    <Box
+      sx={{
+        zIndex: 1,
+        position: 'relative',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+      }}
+    >
       <Lightbox
-        carousel={{
-          finite: true
-        }}
-        on={{
-          click: (props) => updatePreviewPhoto(props.index),
-        }}
-        index={photos.findIndex(p => p === photo)}
-        plugins={[Fullscreen, Slideshow, Zoom]}
         open={showWindow}
-        close={() => setSetting(prev => ({ ...prev, lightboxOpen: false }))}
-        slides={[...photos.map(p => ({ src: composeUrl(p, true) }))]}
+        close={() =>
+          setSetting(prev => ({
+            ...prev,
+            lightboxOpen: false,
+          }))
+        }
+        index={initialIndex >= 0 ? initialIndex : 0}
+        slides={slides}
+        carousel={{
+          finite: true,
+          preload: 10,
+        }}
+        plugins={[
+          Fullscreen,
+          Slideshow,
+          Zoom,
+        ]}
+        on={{
+          view: ({ index }) => {
+            const photo = photos[index];
+
+            if (photo) {
+              setPreviewPhotoObj(photo);
+            }
+          },
+        }}
       />
     </Box>
-  </>;
+  );
 }
