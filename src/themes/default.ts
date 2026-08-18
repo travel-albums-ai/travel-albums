23474d2a4e380d57f98023ac758be058d68244dd
practiseo <practiseo@gmail.com>
2026-08-18 14:16:07 +0200

    feat: add fontSize property to typography in clownThemeTokens for improved text readability

--- src/themes/default.ts
+++ src/themes/default.ts
@@ -51,6 +51,7 @@ export const clownThemeTokens = {
     },
     typography: {
       fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      fontSize: 14,
       button: {
         textTransform: 'none',
       }
