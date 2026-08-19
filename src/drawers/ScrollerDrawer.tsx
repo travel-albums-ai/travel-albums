9888dbc2da262aa5762e34417fa9c40a1f2715c6
practiseo <practiseo@gmail.com>
2026-08-19 19:31:05 +0200

    feat: replace AlbumTimeline with AlbumScroller in ScrollerDrawer and integrate AlbumTimeline in AlbumScroller

--- src/drawers/ScrollerDrawer.tsx
+++ src/drawers/ScrollerDrawer.tsx
@@ -3,7 +3,7 @@ import { useFilteredPhotos_GLOBAL } from '@/context/globals/filteredPhotosStore'
 import { useSections_GLOBAL } from '@/context/globals/sectionsStore';
 import { useSelectedStoreSelector } from '@/context/selectedStore';
 import { useSettingsStoreSelector } from '@/context/settingsStore';
-import AlbumTimeline from '@/drawers/scroller/AlbumTimeline';
+import AlbumScroller from '@/drawers/scroller/AlbumScroller';
 import { GalleryPhoto } from '@/lib/galleryData';
 import { useParams } from 'react-router-dom';
 
@@ -22,8 +22,7 @@ export default function ScrollerDrawer() {
 
   return (
     <GenericPanel id="scroller-drawer" defaultTool toolContext={{ showAll, selectedPhotos: selectedPhotos.length > 0, photosIds: photos.map((p: GalleryPhoto) => p.id), selectMode }}>
-      {/* <AlbumScroller photos={photos} columns={4} rows={3} /> */}
-      <AlbumTimeline photos={photos} />
+      <AlbumScroller photos={photos} columns={4} rows={3} />
     </GenericPanel>
   );
 }
--- src/drawers/scroller/AlbumScroller.tsx
+++ src/drawers/scroller/AlbumScroller.tsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import { useSettingsStoreSelector } from '@/context/settingsStore';
 import AlbumScrollerItem from '@/drawers/scroller/AlbumScrollerItem';
+import AlbumTimeline from '@/drawers/scroller/AlbumTimeline';
 import { GalleryPhoto } from '@/lib/galleryData';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 
@@ -320,6 +321,28 @@ export default function AlbumScroller({
             />
           </Box>
         ))}
+      </Box>
+
+      <Box
+        // onPointerDown={onTrackPointerDown}
+        // onPointerMove={onTrackPointerMove}
+        // onPointerUp={onTrackPointerUp}
+        sx={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 42,
+          height: 48,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          zIndex: 199,
+          pointerEvents: 'auto',
+        }}
+      >
+        <AlbumTimeline photos={photos} chunkSize={rows * columns} onIndexChange={(index, chunkIndex) => {
+          setCurrent(chunkIndex);
+        }}   />
       </Box>
 
       <Box
