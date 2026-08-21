00150090597d64cc6815e8f9073293497fbe4ff6
practiseo <practiseo@gmail.com>
2026-08-21 12:09:11 +0200

    feat: optimize AllPhotosGridVirtuoso component by removing unused code and enhancing List memoization

--- src/pages/components/AllPhotosGridVirtuoso.tsx
+++ src/pages/components/AllPhotosGridVirtuoso.tsx
@@ -1,9 +1,8 @@
 import { Box } from '@mui/material';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useMemo, useRef } from 'react';
 import { VirtuosoGrid, VirtuosoGridHandle } from 'react-virtuoso';
 
 import { useAlbumPhotoCardStoreSelector } from '@/context/albumPhotoCardStore';
-import { useSettingsStoreSelector } from '@/context/settingsStore';
 import { GalleryPhoto } from '@/lib/galleryData';
 import AlbumPhotoCard from '../../components/AlbumPhotoCard';
 
@@ -36,20 +35,18 @@ export default function AllPhotosGridVirtuoso({ photos }: Props) {
   const width = useAlbumPhotoCardStoreSelector((state) => state.width);
   const height = useAlbumPhotoCardStoreSelector((state) => state.height);
   const virtuosoRef = useRef<VirtuosoGridHandle>(null);
-  const previewPhotoObj = useSettingsStoreSelector((state) => state.previewPhotoObj)
+  // const previewPhotoObj = useSettingsStoreSelector((state) => state.previewPhotoObj)
 
-  const photosRef = useRef(photos);
-  useEffect(() => {
-    photosRef.current = photos;
-  }, [photos]);
+  // const photosRef = useRef(photos);
+  // photosRef.current = photos;
 
-  useEffect(() => {
-    if (!previewPhotoObj) return;
-    const index = photosRef.current.findIndex((p) => p.id === previewPhotoObj.id);
-    if (index >= 0) {
-      virtuosoRef.current?.scrollToIndex({ index, align: 'center', behavior: 'auto' });
-    }
-  }, [previewPhotoObj]);
+  // useEffect(() => {
+  //   if (!previewPhotoObj) return;
+  //   const index = photosRef.current.findIndex((p) => p.id === previewPhotoObj.id);
+  //   if (index >= 0) {
+  //     virtuosoRef.current?.scrollToIndex({ index, align: 'center', behavior: 'auto' });
+  //   }
+  // }, [previewPhotoObj]);
 
 
   const itemContent = useCallback(
@@ -67,7 +64,10 @@ export default function AllPhotosGridVirtuoso({ photos }: Props) {
     [photos]
   );
 
-  const List = (props: any) => <GridList {...props} width={width} gap={1} />;
+  const List = useMemo(() => {
+    const Comp = (props: any) => <GridList {...props} width={width} gap={1} />;
+    return Comp;
+  }, [width]);
 
   return (
     <VirtuosoGrid
